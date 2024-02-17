c753723febed0d3d38f9460a32482e8d54f726ad
irene <panisirene@gmail.com>
2024-02-17 13:33:23 -0800

    create navbar and remove vite css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+  return (
+    <nav className="nav-wrapper bg-white flex w-full h-16 justify-center items-center relative">
+      <img src='/TT_logo.png' height="50" width="50" className="absolute left-10"></img>
+      <ul className="nav-items text-black uppercase flex gap-10">
+        <li>Home</li>
+        <li>Esports</li>
+        <li>Content</li>
+        <li>Merch</li>
+      </ul>
+    </nav>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { Navbar } from "../components/Navbar"
+
+export const Home = () => {
+  return (
+    <div className="bg-gray-400 w-full h-full">
+      <Navbar/>
+    </div>
+  )
+}
