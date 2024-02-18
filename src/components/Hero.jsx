1f3452f7de3f691141311b3e63a06b8716b29e23
irene <panisirene@gmail.com>
2024-02-17 20:34:24 -0800

    add text and button to hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 export const Hero = () => {
   return (
-    <div className='hero-wrapper bg-hero bg-cover flex flex-grow bg-top w-full overflow-hidden'>
+    <div className='hero-wrapper bg-hero bg-cover flex flex-grow bg-top w-full overflow-hidden justify-center items-center flex-col text-white gap-5'>
+      <h2 className="uppercase font-bold text-7xl drop-shadow-md">Be the very best</h2>
+      <button type="button" className="bg-black py-3 px-4 text-2xl hover:bg-white hover:text-black duration-300 ease-in-out border border-black">SHOP 100T x POKÉMON</button>
     </div>
   )
 }
