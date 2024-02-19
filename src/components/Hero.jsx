ed509708eb275ce26907ef33b6488cb40fee0dce
irene <panisirene@gmail.com>
2024-02-18 16:13:39 -0800

    tweak positioning of text on hero img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,10 @@
 export const Hero = () => {
   return (
-    <div className='hero-wrapper bg-hero bg-cover flex flex-grow w-full flex flex-col justify-center items-center gap-2 overflow-hidden text-white saturate-[1.25]'>
-      <h2 className="uppercase font-bold text-8xl drop-shadow-lg tracking-wider bg-black/5">Think Thieves</h2>
-      <span className="lowercase text-2xl drop-shadow-lg italic bg-black/5">Esports // Content // Apparel</span>
+    <div className='hero-wrapper bg-hero bg-cover flex flex-grow w-full gap-2 overflow-hidden text-white saturate-[1.25]'>
+      <div className="content h-full w-full flex justify-between items-center mx-10">
+        <h2 className="uppercase font-bold text-8xl drop-shadow-lg tracking-wider">Think</h2>
+        <h2 className="uppercase font-bold text-8xl drop-shadow-lg tracking-wider">Thieves</h2>
+      </div>
     </div>
   )
 }
