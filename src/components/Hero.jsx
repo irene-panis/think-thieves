c80318a13332a9ff4a25049d18c9a14d643a69b1
irene <panisirene@gmail.com>
2024-02-18 16:09:11 -0800

    tweak hero text visibility + hover bg nav links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
 export const Hero = () => {
   return (
-    <div className='hero-wrapper bg-hero bg-cover flex flex-grow bg-top w-full overflow-hidden justify-center items-center flex-col text-white gap-2'>
-      <h2 className="uppercase font-bold text-8xl drop-shadow-lg tracking-wider">Think Different</h2>
-      <span className="lowercase text-2xl drop-shadow-lg italic">Esports // Content // Apparel</span>
+    <div className='hero-wrapper bg-hero bg-cover flex flex-grow w-full flex flex-col justify-center items-center gap-2 overflow-hidden text-white saturate-[1.25]'>
+      <h2 className="uppercase font-bold text-8xl drop-shadow-lg tracking-wider bg-black/5">Think Thieves</h2>
+      <span className="lowercase text-2xl drop-shadow-lg italic bg-black/5">Esports // Content // Apparel</span>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ export const Navbar = () => {
   return (
     <nav className="nav-wrapper bg-white flex w-full h-16 justify-center items-center relative z-10 shadow-md">
       <img src='/TT_logo.png' height="50" width="50" className="absolute left-10"></img>
-      <ul className="nav-items text-black uppercase flex gap-10">
-        <li>Home</li>
-        <li>Esports</li>
-        <li>Content</li>
-        <li>Merch</li>
+      <ul className="nav-items text-black uppercase flex gap-16">
+        <li className="px-1 hover:underline hover:bg-black hover:text-white">Home</li>
+        <li className="px-1 hover:underline hover:bg-black hover:text-white">Esports</li>
+        <li className="px-1 hover:underline hover:bg-black hover:text-white">Content</li>
+        <li className="px-1 hover:underline hover:bg-black hover:text-white">Merch</li>
       </ul>
     </nav>
   )
