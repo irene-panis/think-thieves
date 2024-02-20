3da8ce8f48d9ad444228c4fce7b5587ac6aea6ff
irene <panisirene@gmail.com>
2024-02-19 20:23:55 -0800

    return no matches found if matchList empty

--- server/utils/valscraper.js
+++ server/utils/valscraper.js
@@ -7,7 +7,7 @@ const getValMatches = async () => {
   
   // https://www.vlr.gg/team/matches/120/100-thieves/?group=completed
   // https://www.vlr.gg/team/matches/120/100-thieves/?group=upcoming
-  const url = 'https://www.vlr.gg/team/matches/120/100-thieves/?group=completed';
+  const url = 'https://www.vlr.gg/team/matches/120/100-thieves/?group=upcoming';
 
   await page.goto(url, {
     waitUntil: "domcontentloaded",
@@ -15,6 +15,9 @@ const getValMatches = async () => {
 
   const matches = await page.evaluate(() => {
     const matchList = Array.from(document.querySelectorAll("a.wf-card.m-item")).slice(0, 10);
+
+    if (matchList.length === 0) { return 'No matches found'; }
+
     const matchData = [];
 
     matchList.forEach((match) => {
