eb0722b337a65cb3c3b7442ec558186c081babf2
irene <panisirene@gmail.com>
2024-02-20 18:49:06 -0800

    change bg white

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { Home } from './pages/Home'
 function App() {
 
   return (
-    <div className="w-full h-screen font-dmmono">
+    <div className="w-full font-dmmono bg-[#F4F3F2]">
       <Home/>
     </div>
   )
