803049f636cf03896d206dfce1ca2aba557461d8
irene <panisirene@gmail.com>
2024-02-21 21:57:06 -0800

    make text responsive with clamp()

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import { Header } from "./Header";
 import { Image } from "./Image";
 import { Subheader } from "./Subheader";
@@ -5,22 +6,22 @@ import { Subheader } from "./Subheader";
 export const About = () => {
   return (
     <div className="about-wrapper flex justify-center items-center h-screen gap-20 p-20">
-      <div className="column-one w-1/2 h-full flex flex-col">
-        <div className="text h-1/3 flex flex-col justify-center gap-5">
+      <div className="column-one w-1/2 h-full flex flex-col gap-3">
+        <div className="text flex-grow flex flex-col justify-center gap-3">
           <Header
-            header="You're in thief territory now."
-            divStyles="justify-end text-right"
-            textStyles="w-2/3"
+            header={["You're in ", <br/>, "thief territory now."]}
           />
           <Subheader
             content="The prime destination for esports and gaming content."
             styles="justify-end text-md"
           />
         </div>
-        <Image
-          src="/codchamps.jpeg"
-          styles="w-full h-2/3 overflow-hidden"
-        />
+        <div className="image-wrapper flex-grow overflow-hidden">
+          <Image
+            src="/codchamps.jpeg"
+            styles="w-full h-full object-cover"
+          />
+        </div>
       </div>
       <div className="column-two w-1/2 h-full">
         <Image
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = ( props ) => {
   return (
     <div className={`text-wrapper flex ${props.divStyles}`}>
-      <h2 className={`uppercase text-7xl font-bold font-oswald ${props.textStyles}`}>{props.header}</h2>
+      <h2 className={`uppercase font-bold font-oswald ${props.textStyles} leading-none`}>{props.header}</h2>
     </div>
   )
 }
