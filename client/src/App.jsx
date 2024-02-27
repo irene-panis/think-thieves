55763de1b8d7b9c66110d46e085d86867d2a5db3
irene <panisirene@gmail.com>
2024-02-26 22:42:53 -0800

    add links to nav and conditionally render nav w/ hero on homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,18 +6,13 @@ import { useState, useEffect } from 'react';
 import { Hero } from './components/Hero';
 
 function App() {
-
-  const [page, setPage] = useState('');
   const current = useLocation().pathname;
-
-  useEffect(() => {
-    setPage(current);
-  }, [current]);
+  const isHomePage = current === '/';
 
   return (
     <div className="wrapper bg-gray-400 flex flex-col w-full">
       {
-        page === '/' ?
+        isHomePage ?
         (
           <div className="flex flex-col w-full h-screen relative">
             <Navbar/>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,38 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
   return (
     <nav className="nav-wrapper bg-white flex w-full h-16 justify-center items-center relative z-10 shadow-md">
       <img src='/TT_logo.png' height="50" width="50" className="absolute left-10"></img>
       <ul className="nav-items text-black uppercase flex gap-16">
-        <li className="px-1 hover:underline hover:bg-black hover:text-white">Home</li>
-        <li className="px-1 hover:underline hover:bg-black hover:text-white">Esports</li>
-        <li className="px-1 hover:underline hover:bg-black hover:text-white">Content</li>
-        <li className="px-1 hover:underline hover:bg-black hover:text-white">Merch</li>
+        <li className="px-1 hover:underline hover:bg-black hover:text-white">
+          <Link
+            to="/"
+          >
+            Home
+          </Link>
+        </li>
+        <li className="px-1 hover:underline hover:bg-black hover:text-white">
+          <Link
+            to="/esports"
+          >
+            Esports
+          </Link>
+        </li>
+        <li className="px-1 hover:underline hover:bg-black hover:text-white">
+          <Link
+            to="/content"
+          >
+            Content
+          </Link>
+        </li>
+        <li className="px-1 hover:underline hover:bg-black hover:text-white">
+        <Link
+            to="/apparel"
+          >
+            Apparel
+          </Link>
+        </li>
       </ul>
     </nav>
   )
