e8f4c9a617b569e66374e32f1cfee5952f316090
irene <panisirene@gmail.com>
2024-02-29 18:25:51 -0800

    create apparel pillar using pillar template

--- client/src/components/ApparelPillar.jsx
+++ client/src/components/ApparelPillar.jsx
@@ -1,5 +1,15 @@
+import { PillarTemplate } from "./PillarTemplate"
+
 export const Apparel = () => {
   return (
-    <div id="apparel" className="pt-10">heyyyyyyy</div>
+    <div id="apparel" className="apparel-wrapper h-[85vh]">
+    <PillarTemplate
+      header="Bridging the gap between gaming and fashion."
+      src="poke.jpeg"
+      p1="100 Thieves apparel transcends the boundary of what streetwear can be."
+      p2="Our collaborations with the most iconic names in entertainment today showcase our commitment to merging gaming and popular culture with cutting-edge fashion, creating apparel that speaks to both gamers and fashion enthusiasts alike."
+      linkTo="/apparel"
+    />
+  </div>
   )
 }
