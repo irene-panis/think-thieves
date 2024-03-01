9ab176d53cfaba952558e89f74f8c4c23d2227e6
irene <panisirene@gmail.com>
2024-02-29 21:04:38 -0800

    change culture section layout

--- client/src/components/Culture.jsx
+++ client/src/components/Culture.jsx
@@ -11,23 +11,24 @@ export const Culture = ( ) => {
       <div className="culture-imgs h-screen grid grid-cols-4 grid-rows-3 gap-5">
         <Image
           src="/gucci.jpeg"
-          styles="col-start-3 col-span-2 row-span-2"
+          styles="col-start-1 col-span-2 row-span-2"
         />
         <Image
           src="/trophy.jpeg"
-          styles="col-start-2 col-span-1 row-start-1 row-span-1"
+          styles="col-start-3 col-span-1 row-start-1 row-span-1"
         />
         <Image
           src="/jjk.jpeg"
-          styles="col-start-1 col-span-1 row-start-3 row-span-1"
+          styles="col-start-4 col-span-1 row-start-3 row-span-1"
         />
         <Subheader
           content="Our mission is to give every gamer something to be proud of."
-          divStyles="col-start-1 row-start-2 items-end uppercase"
+          divStyles="col-start-4 row-start-2 items-end uppercase"
+          spanStyles="culture-head"
         />
         <Subheader
           content="We believe in the power of community and the thrill of competition. With a relentless commitment to winning and a dedication to producing top-tier content, we're making a name for ourselves as leaders in the gaming industry, and we're not stopping now."
-          divStyles="col-start-3 col-span-1 row-start-3 uppercase"
+          divStyles="col-start-2 col-span-1 row-start-3 uppercase"
         />
       </div>
     </div>
