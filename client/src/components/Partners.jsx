3099994098bf13fcc8bbdbc4fc17f3ed1f643453
irene <panisirene@gmail.com>
2024-03-03 16:38:06 -0800

    create partners section

--- client/src/components/Partners.jsx
+++ client/src/components/Partners.jsx
@@ -1,11 +1,37 @@
 import { Header } from "./Header"
+import { Image } from "./Image"
+import { Subheader } from "./Subheader"
 
 export const Partners = () => {
   return (
-    <div className="partners-wrapper flex flex-col gap-10 px-20">
+    <div className="partners-wrapper flex flex-col gap-12 px-20">
       <Header
         header="Our Partners"
       />
+      <div className="img-container flex justify-center align-center gap-10">
+        <Image
+          src="att.png"
+          styles="w-auto h-[100px]"
+        />
+        <Image
+          src="gucci.png"
+          styles="w-auto h-[100px]"
+        />
+        <Image
+          src="heineken.png"
+          styles="w-auto h-[100px]"
+        />
+        <Image
+          src="lexus.png"
+          styles="w-auto h-[100px]"
+        />
+      </div>
+      <div className="partners-text">
+        <Subheader
+          content="100 THIEVES is home to brands designed to disrupt the industries they compete in."
+          divStyles="justify-center"
+        />
+      </div>
     </div>
   )
 }
