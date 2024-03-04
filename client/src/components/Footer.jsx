f6e9898df1b3ba7ff06279b58ac4727b55f97d7d
irene <panisirene@gmail.com>
2024-03-03 17:15:19 -0800

    fix spacing in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Subscribe } from "./Subscribe"
 export const Footer = () => {
   return (
     <div className="footer-wrapper h-[200px] flex justify-evenly items-center py-5 bg-[#131200] text-white">
-      <span>made with 🤍 by <a href="https://github.com/irene-panis/think-thieves" target="_blank">irene panis</a>.</span>
+      <span className="w-[40%] text-center">made with 🤍 by <a href="https://github.com/irene-panis/think-thieves" target="_blank" className="underline hover:bg-white hover:text-black duration-200 ease-in-out">irene panis</a>.</span>
       <div className="vl"></div>
       <Subscribe/>
     </div>
