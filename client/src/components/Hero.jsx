aefa4f03ef0c095ebab8278c00c0c55da545cb58
irene <panisirene@gmail.com>
2024-03-05 20:16:15 -0800

    make hero mobile first

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,7 +4,7 @@ export const Hero = () => {
   return (
     <div className='hero-wrapper bg-hero bg-cover bg-center flex flex-grow w-full gap-2 overflow-hidden text-white saturate-[1.25] relative'>
       <Marquee/>
-      <div className="content h-full w-full flex justify-between items-center mx-10">
+      <div className="content h-full w-full flex flex-col justify-center items-center mx-10">
         <h2 className="uppercase font-bold text-8xl drop-shadow-lg tracking-wider">Think</h2>
         <h2 className="uppercase font-bold text-8xl drop-shadow-lg tracking-wider">Thieves</h2>
       </div>
