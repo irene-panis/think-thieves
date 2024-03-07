c521c19bb6f1c3a4e708a38dd1ccdd1baca4b147
irene <panisirene@gmail.com>
2024-03-07 14:24:45 -0800

    fix grid in culture section

--- client/src/components/Culture.jsx
+++ client/src/components/Culture.jsx
@@ -8,27 +8,31 @@ export const Culture = ( ) => {
       <Header
         header="Our Culture"
       />
-      <div className="culture-imgs h-screen grid grid-cols-4 grid-rows-3 gap-5">
+      <div className="culture-imgs h-screen grid grid-cols-2 grid-rows-5 gap-5">
         <Image
           src="/gucci.jpeg"
           styles="col-start-1 col-span-2 row-span-2"
         />
         <Image
           src="/trophy.jpeg"
-          styles="col-start-3 col-span-1 row-start-1 row-span-1"
+          styles="col-start-2 col-span-1 row-start-4 row-span-1"
         />
         <Image
           src="/jjk.jpeg"
-          styles="col-start-4 col-span-1 row-start-3 row-span-1"
+          styles="col-start-4 col-span-1 row-start-3 row-span-1 hidden"
         />
         <Subheader
           content="Our mission is to give every gamer something to be proud of."
-          divStyles="col-start-4 row-start-2 items-end uppercase"
+          divStyles="col-start-1 col-span-2 row-start-3 row-span-1 items-end uppercase"
           spanStyles="culture-head"
         />
         <Subheader
-          content="We believe in the power of community and the thrill of competition. With a relentless commitment to winning and a dedication to producing top-tier content, we're making a name for ourselves as leaders in the gaming industry, and we're not stopping now."
-          divStyles="col-start-2 col-span-1 row-start-3 uppercase"
+          content="We believe in the power of community and the thrill of competition."
+          divStyles="col-start-1 col-span-1 row-start-4 row-span-2 uppercase"
+        />
+        <Subheader
+          content="With a relentless commitment to winning and a dedication to producing top-tier content, we're making a name for ourselves as leaders in the gaming industry, and we're not stopping now."
+          divStyles="col-start-1 col-span-2 row-start-5 row-span-1 uppercase"
         />
       </div>
     </div>
