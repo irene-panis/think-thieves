1c4ce1bfddc86ff7f92a35699cbd90714acd6ed8
irene <panisirene@gmail.com>
2024-03-08 14:33:30 -0800

    make images in partners flex-wrap

--- client/src/components/Partners.jsx
+++ client/src/components/Partners.jsx
@@ -4,11 +4,11 @@ import { Subheader } from "./Subheader"
 
 export const Partners = () => {
   return (
-    <div className="partners-wrapper flex flex-col gap-12 px-20">
+    <div className="partners-wrapper flex flex-col gap-12">
       <Header
         header="Our Partners"
       />
-      <div className="img-container flex justify-center align-center gap-10">
+      <div className="img-container flex flex-wrap justify-center align-center gap-10">
         <Image
           src="att.png"
           styles="w-auto h-[100px]"
@@ -29,7 +29,7 @@ export const Partners = () => {
       <div className="partners-text">
         <Subheader
           content="100 THIEVES is home to brands designed to disrupt the industries they compete in."
-          divStyles="justify-center"
+          divStyles="justify-center text-center"
         />
       </div>
     </div>
