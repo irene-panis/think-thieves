6141d7ce42ae22a11bdd8f115e34880d9e8b0a77
irene <panisirene@gmail.com>
2024-04-14 19:39:43 -0700

    fetch upcoming val matches

--- client/src/components/esports/UpcomingMatches.jsx
+++ client/src/components/esports/UpcomingMatches.jsx
@@ -1,12 +1,32 @@
 import { Subheader } from "../Subheader"
+import { useState, useEffect } from "react";
 
 export const UpcomingMatches = () => {
+
+  const [upcoming, setUpcoming] = useState([]);
+
+  useEffect(() => {
+    const getMatches = async () => {
+      const getURL = "http://localhost:3001/api/get-val";
+      const response = await fetch(getURL, {
+        method: "GET",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        }
+      });
+      const matches = await response.json();
+      setUpcoming(matches);
+    };
+    getMatches();
+  }, []);
+
   return (
     <>
       <Subheader
           content="Upcoming Matches"
       />
-      <span></span>
+      <span>{upcoming}</span>
     </>
   )
 }
