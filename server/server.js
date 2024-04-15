49dbf71d467651056a4f2aa7f88f57e41deb1b42
irene <panisirene@gmail.com>
2024-04-14 19:45:36 -0700

    install cors and revert eslint ver

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import getValMatches from './utils/valscraper.js';
 dotenv.config();
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/api/get-val', async (req, res) => {
   try {
