28959731d63ceda12daba70745c8fb6f86ec569b
irene <panisirene@gmail.com>
2024-04-17 23:00:16 -0700

    create redis client

--- server/server.js
+++ server/server.js
@@ -3,12 +3,17 @@ import dotenv from "dotenv";
 import getValMatches from './utils/valscraper.js';
 dotenv.config();
 import cors from "cors";
+import { createClient } from "redis";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
+
+const client = await createClient()
+  .on('error', err => console.log('Redis Client Error', err))
+  .connect();
 
 app.get('/api/get-val', async (req, res) => {
   try {
