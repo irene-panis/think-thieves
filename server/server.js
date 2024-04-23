275394c4f44f7dee3d29e4808d43eb0bca2708cb
irene <panisirene@gmail.com>
2024-04-22 21:42:42 -0700

    implement redis caching (i failed)

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import getValMatches from './utils/valscraper.js';
 dotenv.config();
 import cors from "cors";
-import { createClient } from "redis";
+import { createClient } from 'redis';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,37 +11,46 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-const client = await createClient()
-  .on('error', err => console.log('Redis Client Error', err))
-  .connect();
+const client = createClient();
+client.on('error', err => console.log('Redis Client Error', err));
+await client.connect();
 
 let matchNumber = 0; // start matchNumber at 0
 // this is for uniquely identifying matches
 // handles edge case of matches against same team on same day etc
 
 app.get('/api/get-val', async (req, res) => {
-  try {
-    client.flushDb(); // start with fresh db (for testing purposes for now)
-    // add redis check and grabbing from cache
-    const data = await getValMatches();
-    data.forEach((match) => {
-      const matchId = match.name + " " + matchNumber;
-      matchNumber++; // count up
-      const matchData = {
-        team: match.name,
-        date: match.date,
-        event: match.event
-      };
-      try {
-        client.hSet(matchId, matchData);
-      } catch (err) {
-        return console.error(err);
-      }
-    });
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  // add redis check and grabbing from cache
+  client.hGetAll("VALORANT", async (err, cache) => {
+    // for some reason this isnt running at all, no console logs showing
+    // but simple sets/gets without callback functions work ???? idk man
+    if (err) console.error(err);
+    if (cache != null) {
+      const matchesArray = JSON.parse(cache.matches);
+      console.log("Cache Hit");
+      return res.json(matchesArray);
+    } else {
+      const data = await getValMatches();
+      const matches = [];
+      data.forEach((match) => {
+        const matchId = match.name + " " + matchNumber;
+        matchNumber++; // count up
+        const matchData = {
+          team: match.name,
+          date: match.date,
+          event: match.event
+        };
+        matches.push({
+          matchId: matchId,
+          matchData: matchData
+        });
+      });
+      client.hSet("VALORANT", "matches", JSON.stringify(matches));
+      client.expire("VALORANT", 60);
+      console.log("Cache Miss");
+      return res.json(data);
+    }
+  })
 });
 
 app.listen(PORT, () => {
