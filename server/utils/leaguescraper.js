4cbfcf4922ad624a70eea16c24338763474bf712
irene <panisirene@gmail.com>
2024-04-29 16:12:50 -0700

    add headers to leaguescraper, add jsdom, add return empty matches array

--- server/utils/leaguescraper.js
+++ server/utils/leaguescraper.js
@@ -1,14 +1,31 @@
+import { JSDOM } from 'jsdom';
+
 const getLeagueMatches = async () => {
   // grab data
+  // test endpoint for populated upcoming matches container:
+  // https://liquipedia.net/leagueoflegends/api.php?action=parse&format=json&page=WAP%20Esports&prop=text
   const endpoint = 'https://liquipedia.net/leagueoflegends/api.php?action=parse&format=json&page=100%20Thieves&prop=text';
-  const response = await fetch(endpoint);
+  const response = await fetch(endpoint, {
+    method: 'GET',
+    headers: {
+      'User-Agent': '100T Fan Site (github.com/irene-panis, irenempanis@gmail.com)',
+      'Accept-Encoding': 'gzip'
+    }
+  });
   const data = await response.json();
+
+  // create a virtual DOM environment
+  const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
+  const { document } = dom.window;
 
   // table parsing
   const parsedText = data.parse.text['*'];
   var content = document.createElement('div'); // create dummy DOM element
   content.innerHTML = parsedText;
   const upcomingMatches = content.querySelector('.fo-nttax-infobox.panel'); // upcoming matches container
+
+  if (!upcomingMatches) return []; // if upcoming matches container not found just return empty array
+
   const matchesArray = upcomingMatches.querySelectorAll('tbody'); // all individual match rows
 
   const matches = []; // array to push data into
@@ -35,7 +52,6 @@ const getLeagueMatches = async () => {
     matches.push(matchObj);
   });
 
-  console.log(matches);
   return matches;
 };
 
