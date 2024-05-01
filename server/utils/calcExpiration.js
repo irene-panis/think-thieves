d396d516007d3d995dbf76c49658890832e8c8b0
irene <panisirene@gmail.com>
2024-05-01 11:44:55 -0700

    create func for calculating time until input date (for redis caching)

---
+++ server/utils/calcExpiration.js
@@ -0,0 +1,6 @@
+import convertToUtc from "./utcConverter"
+
+export const calcExpiration = (date, tz) => {
+  const utc = convertToUtc(date, tz);
+  return (new Date(utc) - new Date()) / 1000; // time until input date in seconds
+}
