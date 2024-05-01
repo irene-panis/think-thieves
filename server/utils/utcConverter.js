a3e8c1c3278f5ddffeaa3d6db647a1e53014b410
irene <panisirene@gmail.com>
2024-04-30 17:13:26 -0700

    fix utc converter

--- server/utils/utcConverter.js
+++ server/utils/utcConverter.js
@@ -1,9 +1,6 @@
 const convertToUtc = (input) => {
   // assuming incoming date is from +2 timezone
-  const date = new Date(input);
-  const timezoneOffset = 2;
-  const utcTime = new Date(date.getTime() - timezoneOffset * 60 * 60 * 1000);
-  return utcTime;
+  return new Date(input + " GMT+0200");
 }
 
 export default convertToUtc;
