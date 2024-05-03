f014b09c8ce69806a60bc4b94a6cc8c8f5b621b6
irene <panisirene@gmail.com>
2024-05-03 03:12:42 -0700

    grab twitch token to access API

---
+++ server/utils/getTwitchToken.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const getTwitchToken = async () => {
+  const url = "https://id.twitch.tv/oauth2/token";
+  const CLIENT_ID = process.env.TWITCH_CLIENT_ID;
+  const CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET;
+  const body = `client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: body,
+  });
+  const data = await response.json();
+  return data;
+}
