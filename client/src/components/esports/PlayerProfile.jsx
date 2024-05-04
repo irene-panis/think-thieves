c79a8809211ed1fdd3696c53c684b62790e712f4
irene <panisirene@gmail.com>
2024-05-03 17:14:32 -0700

    render social username instead of entire social link

--- client/src/components/esports/PlayerProfile.jsx
+++ client/src/components/esports/PlayerProfile.jsx
@@ -1,15 +1,15 @@
-import { Subheader } from "../Subheader"
-import { useState, useEffect } from "react"
+import { Subheader } from "../Subheader";
+import { useState, useEffect } from "react";
 import { Image } from "../Image";
 import { SocialIcon } from "../SocialIcon";
+import { Header } from "../Header";
 
-export const PlayerProfile = ( props ) => {
-
+export const PlayerProfile = (props) => {
   const [player, setPlayer] = useState(props.roster[0]);
 
   const handlePlayerChange = (newPlayer) => {
     setPlayer(newPlayer);
-  }
+  };
 
   useEffect(() => {
     setPlayer(props.roster[0]);
@@ -18,37 +18,37 @@ export const PlayerProfile = ( props ) => {
   const playerSocials = player.socials;
 
   return (
-    <div>
-      <Subheader
-        content="PLAYER PROFILES"
-      />
+    <div className="flex flex-col">
+      <Subheader content="PLAYER PROFILES" />
       <div className="players flex gap-2">
-        {
-          props.roster.map((player, index) => (
-            <button 
-              type="button"
-              onClick={() => handlePlayerChange(player)} 
-              key={index}
-              className="bg-white py-1 px-2 border border-black uppercase hover:bg-black hover:text-white ease-in-out duration-200"
-            >
-              {player.ign}
-            </button>
-          ))
-        }
+        {props.roster.map((player, index) => (
+          <button
+            type="button"
+            onClick={() => handlePlayerChange(player)}
+            key={index}
+            className="bg-white py-1 px-2 border border-black uppercase hover:bg-black hover:text-white ease-in-out duration-200"
+          >
+            {player.ign}
+          </button>
+        ))}
       </div>
-      <div className="player-profile flex flex-col">
-        <span>Current player: {player.ign}</span>
-        {
-          playerSocials.map((social, index) => (
-            <span key={index}>
-              <SocialIcon platform={social.platform}/>: {social.link}
-            </span>
-          ))
-          }
-        <Image
-          src={player.img}
-        />
+      <div className="player-profile flex flex-col my-4">
+        <Header header={player.ign} />
+        <div className="player-socials flex gap-4">
+          {playerSocials.map((social, index) => {
+            const username = social.link.split("/").pop(); // Get the last part of the URL
+            return (
+              <span key={index}>
+                <a href={social.link}>
+                  <SocialIcon platform={social.platform} />
+                  {username}
+                </a>
+              </span>
+            );
+          })}
+        </div>
+        <Image src={player.img} />
       </div>
     </div>
-  )
-}
+  );
+};
