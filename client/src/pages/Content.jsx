d370a9cfcf42215b6651ca40d772331ee03d2c69
irene <panisirene@gmail.com>
2024-05-04 16:48:57 -0700

    track state of current creator

--- client/src/pages/Content.jsx
+++ client/src/pages/Content.jsx
@@ -1,11 +1,19 @@
 import { Creators } from "../components/content/Creators";
+import { useState } from "react";
 
 export const Content = () => {
   const containerHeight = `calc(100vh - 4rem)`;
 
+  const [creator, setCreator] = useState('Nadeshot');
+
+  const handleCreatorChange = (newCreator) => {
+    setCreator(newCreator);
+  } 
+
+
   return (
     <div className="content-wrapper w-full h-full" style={{ height: containerHeight }}>
-      <Creators/>
+      <Creators onCreatorChange={handleCreatorChange}/>
     </div>
   )
 }
