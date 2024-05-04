463cfad5fec94cbf7e9a2372a79ebc512b36ee83
irene <panisirene@gmail.com>
2024-05-04 16:32:51 -0700

    add creators scroller to content page

--- client/src/pages/Content.jsx
+++ client/src/pages/Content.jsx
@@ -1,9 +1,11 @@
+import { Creators } from "../components/content/Creators";
+
 export const Content = () => {
   const containerHeight = `calc(100vh - 4rem)`;
 
   return (
     <div className="content-wrapper w-full h-full" style={{ height: containerHeight }}>
-      <span>content tab</span>
+      <Creators/>
     </div>
   )
 }
