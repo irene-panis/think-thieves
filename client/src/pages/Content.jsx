0ce2b99b922acf747c01e8d01b678f3053bdfd43
irene <panisirene@gmail.com>
2024-05-04 17:53:01 -0700

    import creators array into Content page

--- client/src/pages/Content.jsx
+++ client/src/pages/Content.jsx
@@ -1,10 +1,11 @@
 import { Creators } from "../components/content/Creators";
 import { useState } from "react";
+import { creators } from "../../../server/utils/creatorData";
 
 export const Content = () => {
   const containerHeight = `calc(100vh - 4rem)`;
 
-  const [creator, setCreator] = useState('Nadeshot');
+  const [creator, setCreator] = useState(creators[0]);
 
   const handleCreatorChange = (newCreator) => {
     setCreator(newCreator);
