f2e672fe24e21d6feda84d336b71f50d2e358879
irene <panisirene@gmail.com>
2024-05-05 00:04:54 -0700

    make scrollbar not vertically scrollable

--- client/src/components/content/Creators.jsx
+++ client/src/components/content/Creators.jsx
@@ -4,12 +4,12 @@ export const Creators = ( {onCreatorChange} ) => {
 
   return (
     <div className="flex items-center justify-center py-10">
-      <ol className="uppercase font-bold overflow-x-auto flex gap-4">
+      <ol className="uppercase font-bold overflow-x-auto overflow-y-hidden flex gap-4">
         {creators.map((creator, index) => (
           <li 
             key={index} 
             onClick={() => onCreatorChange(creator)}
-            className="font-oswald text-5xl"
+            className="font-oswald text-5xl hover:text-zinc-500 duration-200 ease-in-out hover:translate-y-2 p-2"
           >
             {creator.name}
           </li>
