86132db0864697e9ec13e8073d741ebad1032cad
irene <panisirene@gmail.com>
2024-05-04 17:51:35 -0700

    add creator data in an array

---
+++ server/utils/creatorData.js
@@ -0,0 +1,233 @@
+export const creators = [
+  {
+    name: 'Nadeshot',
+    img: '/content/nadeshot.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/Nadeshot' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/nadeshot' },
+      { platform: 'instagram', link: 'https://www.instagram.com/nadeshot/' },
+      { platform: 'youtube', link: 'https://www.youtube.com/@Nadeshot/' }
+    ]
+  },
+  {
+    name: 'Valkyrae',
+    img: '/content/valkyrae.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/Valkyrae' },
+      { platform: 'instagram', link: 'https://www.instagram.com/valkyrae' },
+      { platform: 'youtube', link: 'https://www.youtube.com/@Valkyrae' },
+      { platform: 'tiktok', link: 'https://www.tiktok.com/@valkyrae' }
+    ]
+  },
+  {
+    name: 'Courage',
+    img: '/content/courage.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/CouRageJD' },
+      { platform: 'instagram', link: 'https://www.instagram.com/CouRageJD/' },
+      { platform: 'youtube', link: 'https://www.youtube.com/CouRageJD' },
+    ]
+  },
+  {
+    name: 'Brooke',
+    img: '/content/brookeab.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/brookeab' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/brookeab' },
+      { platform: 'instagram', link: 'https://www.instagram.com/brookeabb' },
+      { platform: 'youtube', link: 'https://www.youtube.com/@BrookeAB' },
+      { platform: 'tiktok', link: 'https://www.tiktok.com/@brookeab' },
+    ]
+  },
+  {
+    name: 'd4vd',
+    img: '/content/d4vd.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/d4vddd' },
+      { platform: 'instagram', link: 'https://www.instagram.com/d4vddd/' },
+      { platform: 'tiktok', link: 'https://www.tiktok.com/@d4vdd' }
+    ]
+  },
+  {
+    name: 'Enable',
+    img: '/content/enable.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/Enable' }
+    ]
+  },
+  {
+    name: 'Fuslie',
+    img: '/content/fuslie.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/fuslie' },
+      { platform: 'instagram', link: 'https://www.instagram.com/fuslie/' },
+      { platform: 'youtube', link: 'https://www.youtube.com/fuslie' },
+      { platform: 'tiktok', link: 'https://www.instagram.com/fuslie/' }
+    ]
+  },
+  {
+    name: 'Hiko',
+    img: '/content/hiko.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/Hiko' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/hiko' },
+      { platform: 'youtube', link: 'https://www.youtube.com/c/Hiko' }
+    ]
+  },
+  {
+    name: 'iiTzTimmy',
+    img: '/content/iitztimmy.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/iiTzTimmy' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/iitztimmy' },
+      { platform: 'instagram', link: 'https://www.instagram.com/iitztimmy' },
+      { platform: 'youtube', link: 'https://www.youtube.com/c/iiTzTimmy' }
+    ]
+  },
+  {
+    name: 'JHB',
+    img: '/content/jhb.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/JhbTeam' },
+      { platform: 'instagram', link: 'https://www.instagram.com/jhbteamm' },
+      { platform: 'youtube', link: 'https://www.youtube.com/JhbTeam' },
+    ]
+  },
+  {
+    name: 'Jesser',
+    img: '/content/jesser.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/Jesser' },
+      { platform: 'instagram', link: 'https://www.instagram.com/jesser/' },
+      { platform: 'youtube', link: 'https://www.youtube.com/user/jesserplays' }
+    ]
+  },
+  {
+    name: 'Jiedel',
+    img: '/content/jiedel.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/jiedel' },
+      { platform: 'instagram', link: 'https://www.instagram.com/jiedel/' },
+      { platform: 'youtube', link: 'https://www.youtube.com/@Jiedel' },
+    ]
+  },
+  {
+    name: 'JoshChx',
+    img: '/content/joshchx.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/joshchx' },
+      { platform: 'tiktok', link: 'https://www.tiktok.com/@joshchx' },
+    ]
+  },
+  {
+    name: 'LEGIQN',
+    img: '/content/legiqn.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/LEGIQN' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/legiqn' },
+      { platform: 'instagram', link: 'https://www.instagram.com/legiqn' },
+      { platform: 'youtube', link: 'https://www.youtube.com/@LEGIQN' },
+    ]
+  },
+  {
+    name: 'Lidyuh',
+    img: '/content/lidyuh.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/itslidyuh' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/lidyuh' }
+    ]
+  },
+  {
+    name: 'Lough',
+    img: '/content/brookeab.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/loughtv' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/loughh' },
+      { platform: 'instagram', link: 'https://www.instagram.com/loganlrobles' },
+      { platform: 'youtube', link: 'https://www.youtube.com/@loughh' },
+      { platform: 'tiktok', link: 'https://www.tiktok.com/@loughh' },
+    ]
+  },
+  {
+    name: 'NiceWigg',
+    img: '/content/nicewigg.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/NiceWigg' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/NiceWigg' },
+    ]
+  },
+  {
+    name: 'NoahJ',
+    img: '/content/noahj.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/noahj456' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/NoahJ456' },
+      { platform: 'instagram', link: 'https://www.instagram.com/NoahJ456/' },
+      { platform: 'youtube', link: 'https://www.youtube.com/@NoahJ456' },
+    ]
+  },
+  {
+    name: 'Octane',
+    img: '/content/octane.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/OctaneSam' },
+    ]
+  },
+  {
+    name: 'PeterPark',
+    img: '/content/peterpark.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/peterparkTV' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/peterpark' },
+      { platform: 'instagram', link: 'https://www.instagram.com/peterparktv' }
+    ]
+  },
+  {
+    name: 'Sparkles',
+    img: '/content/sparkles.jpeg',
+    socials: [
+      { platform: 'twitch', link: 'https://www.twitch.tv/sparkles_qt' },
+      { platform: 'instagram', link: 'https://www.instagram.com/sparkles_qt' },
+      { platform: 'youtube', link: 'https://www.youtube.com/@Sparklesqt' }
+    ]
+  },
+  {
+    name: 'Vinnie',
+    img: '/content/vinnie.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/vinniehacker' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/vinnie' },
+      { platform: 'instagram', link: 'https://www.instagram.com/vinniehacker/' },
+      { platform: 'youtube', link: 'https://www.youtube.com/@vinniehacker1702' },
+      { platform: 'tiktok', link: 'https://www.tiktok.com/@vhackerr' },
+    ]
+  },
+  {
+    name: 'Woolz',
+    img: '/content/woolz.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/kingwoolz' },
+      { platform: 'instagram', link: 'https://www.instagram.com/kingwoolz/' },
+      { platform: 'youtube', link: 'https://www.youtube.com/@KingWoolz' },
+      { platform: 'tiktok', link: 'https://www.tiktok.com/@kingwoolz' }
+    ]
+  },
+  {
+    name: 'Yassuo',
+    img: '/content/yassuo.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/Yassuo' },
+      { platform: 'twitch', link: 'https://www.twitch.tv/yassuo' },
+      { platform: 'instagram', link: 'https://www.instagram.com/yassuo/' }
+    ]
+  },
+  {
+    name: 'ZackTTG',
+    img: '/content/zackttg.jpeg',
+    socials: [
+      { platform: 'twitter', link: 'https://twitter.com/ZackTTG' },
+      { platform: 'instagram', link: 'https://www.instagram.com/zackttg/' },
+      { platform: 'youtube', link: 'https://www.youtube.com/@ZackTTG' }
+    ]
+  },
+]
