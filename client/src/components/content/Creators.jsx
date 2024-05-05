a1ccec07611a1722264c1b1bf561f9ce68c397f3
irene <panisirene@gmail.com>
2024-05-04 17:58:19 -0700

    import creators array and add creator change handler onclick

--- client/src/components/content/Creators.jsx
+++ client/src/components/content/Creators.jsx
@@ -1,11 +1,14 @@
-export const Creators = () => {
-  const creators = ["Nadeshot", "Valkyrae", "Courage", "Brooke", "d4vd", "Enable", "Fuslie", "Hiko", "iiTzTimmy", "JHB", "Jesser", "Jiedel", "JoshChx", "LEGIQN", "Lidyuh", "Lough", "NiceWigg", "NoahJ", "Octane", "PeterPark", "Sparkles", "Vinnie", "Woolz", "Yassuo", "ZackTTG"];
+import { creators } from "../../../../server/utils/creatorData";
+
+export const Creators = ( onCreatorChange ) => {
 
   return (
     <div>
       <ol className="uppercase overflow-x-auto flex gap-4">
         {creators.map((creator, index) => (
-          <li key={index}>{creator}</li>
+          <li key={index} onClick={() => onCreatorChange(creator)}>
+            {creator.name}
+          </li>
         ))}
       </ol>
     </div>
