0e70f827176ba77848f1e46bce1801cf082aa24f
irene <panisirene@gmail.com>
2024-05-05 00:06:25 -0700

    hide scrollbar

--- client/src/components/content/Creators.jsx
+++ client/src/components/content/Creators.jsx
@@ -4,7 +4,7 @@ export const Creators = ( {onCreatorChange} ) => {
 
   return (
     <div className="flex items-center justify-center py-10">
-      <ol className="uppercase font-bold overflow-x-auto overflow-y-hidden flex gap-4">
+      <ol className="uppercase font-bold overflow-x-auto overflow-y-hidden disable-scrollbars flex gap-4">
         {creators.map((creator, index) => (
           <li 
             key={index} 
