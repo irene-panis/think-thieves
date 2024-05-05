a992c18e5357e11afef1bff6ce3a11cd648be6c8
irene <panisirene@gmail.com>
2024-05-04 23:51:11 -0700

    add margins

--- client/src/components/content/CreatorSpotlight.jsx
+++ client/src/components/content/CreatorSpotlight.jsx
@@ -2,7 +2,7 @@ import { Image } from "../Image";
 
 export const CreatorSpotlight = ( { creator } ) => {
   return (
-    <div className="creator-spotlight">
+    <div className="creator-spotlight mx-4">
       <Image src={creator.img}/>
       <span>{creator.name}</span>
     </div>
