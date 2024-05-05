4dde27db7c013518b6da84641e62ede0d488ddd7
irene <panisirene@gmail.com>
2024-05-04 23:50:19 -0700

    tweak font size

--- client/src/components/content/Creators.jsx
+++ client/src/components/content/Creators.jsx
@@ -3,13 +3,13 @@ import { creators } from "../../../../server/utils/creatorData";
 export const Creators = ( {onCreatorChange} ) => {
 
   return (
-    <div>
+    <div className="flex items-center justify-center">
       <ol className="uppercase font-bold overflow-x-auto flex gap-4">
         {creators.map((creator, index) => (
           <li 
             key={index} 
             onClick={() => onCreatorChange(creator)}
-            className="font-oswald"
+            className="font-oswald text-5xl"
           >
             {creator.name}
           </li>
