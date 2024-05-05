6f07b9501dafd2717e67f2c5fb2c62e56a612449
irene <panisirene@gmail.com>
2024-05-05 15:11:59 -0700

    add social icons for all creators

--- client/src/components/content/CreatorSpotlight.jsx
+++ client/src/components/content/CreatorSpotlight.jsx
@@ -1,10 +1,24 @@
 import { Image } from "../Image";
+import { SocialIcon } from "../SocialIcon";
 
 export const CreatorSpotlight = ( { creator } ) => {
   return (
     <div className="creator-spotlight mx-4 flex-grow">
       <Image src={creator.img}/>
-      <span className="uppercase">{creator.name}</span>
+      <div className="creator-info flex justify-between">
+        <span className="uppercase">{creator.name}</span>
+        <div className="creator-socials">
+          {creator.socials.map((social, index) => {
+            return (
+              <span key={index}>
+                <a href={social.link}>
+                  <SocialIcon platform={social.platform}/>
+                </a>
+              </span>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
