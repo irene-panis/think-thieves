a1b2d7ede4c64978ce06ca2340737fcd243f2f2b
irene <panisirene@gmail.com>
2024-05-04 19:27:53 -0700

    add creator images

--- client/src/components/content/CreatorSpotlight.jsx
+++ client/src/components/content/CreatorSpotlight.jsx
@@ -1,6 +1,9 @@
+import { Image } from "../Image";
+
 export const CreatorSpotlight = ( { creator } ) => {
   return (
     <div className="creator-spotlight">
+      <Image src={creator.img}/>
       <span>{creator.name}</span>
     </div>
   )
