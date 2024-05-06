9a85b781f2abc19eea71077d9185a21704fb7002
irene <panisirene@gmail.com>
2024-05-05 20:00:12 -0700

    remove border radius on buttons

--- client/src/components/esports/TeamButton.jsx
+++ client/src/components/esports/TeamButton.jsx
@@ -1,7 +1,7 @@
 export const TeamButton = ( {onTeamChange, children} ) => {
   return (
     <>
-      <button type="button" className="border border-grey bg-white text-black py-1 px-4 rounded-full shadow-sm active:scale-90 duration-100 ease-in-out" onClick={() => onTeamChange(`${children}`)}>{children}</button>
+      <button type="button" className="border border-black bg-white text-black py-1 px-4 active:scale-90 hover:text-white hover:bg-black hover:border-white duration-100 ease-in-out" onClick={() => onTeamChange(`${children}`)}>{children}</button>
     </>
   )
 }
