18c65c6382ce52c5267a45cf0b9a6acfdb45ff4f
irene <panisirene@gmail.com>
2024-05-06 14:35:37 -0700

    adjust margins

--- client/src/components/content/CreatorFeed.jsx
+++ client/src/components/content/CreatorFeed.jsx
@@ -2,7 +2,7 @@ import { WhosLive } from "../esports/WhosLive"
 
 export const CreatorFeed = () => {
   return (
-    <div className="creator-feed flex flex-wrap m-2">
+    <div className="creator-feed flex flex-wrap m-4">
       <WhosLive team="CONTENT"/>
     </div>
   )
