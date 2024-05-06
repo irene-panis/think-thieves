92084453ee97e58201efe925f35a70485199a1d5
irene <panisirene@gmail.com>
2024-05-05 21:33:08 -0700

    add bios and titles for creators

--- server/utils/creatorData.js
+++ server/utils/creatorData.js
@@ -1,16 +1,20 @@
 export const creators = [
   {
     name: 'Nadeshot',
+    title: 'CEO',
+    bio: 'THE GAMING LIFESTYLE VISIONARY, NADESHOT IS THE EMBODIMENT OF EVERY GAMER’S DREAM. HUSTLING FOR THE LAST 11 YEARS, NADESHOT HAS BEEN A GAME CHANGER SINCE HE STEPPED ON THE SCENE. FROM CALL OF DUTY PLAYER, TO STREAMER, TO CONTENT CREATOR, AND CEO OF 100 THIEVES.',
     img: '/content/nadeshot.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/Nadeshot' },
       { platform: 'twitch', link: 'https://www.twitch.tv/nadeshot' },
       { platform: 'instagram', link: 'https://www.instagram.com/nadeshot/' },
       { platform: 'youtube', link: 'https://www.youtube.com/@Nadeshot/' }
-    ]
+    ],
   },
   {
     name: 'Valkyrae',
+    title: 'Co-owner',
+    bio: '2020 CONTENT CREATOR OF THE YEAR, THE TOP FEMALE STREAMER ON THE INTERNET, VIDEO VIXEN, VOICE OVER ACTOR, AND CO-OWNER OF 100 THIEVES.',
     img: '/content/valkyrae.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/Valkyrae' },
@@ -20,6 +24,8 @@ export const creators = [
   },
   {
     name: 'Courage',
+    title: 'Co-owner',
+    bio: "HOST, STREAMER, CO-OWNER OF 100THIEVES, THERE'S NOTHING THAT COURAGE CAN'T DO. HE'S BEEN VOTED MOST HANDSOME AND TALLEST STREAMER AMONG THE 100T CREATORS.",
     img: '/content/courage.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/CouRageJD' },
@@ -29,6 +35,8 @@ export const creators = [
   },
   {
     name: 'Brooke',
+    title: 'Creator',
+    bio: `FUN LOVING, BUBBLY, SMART, AND QUITE THE BIBLIOPHILE. YOU CAN CATCH MORE OF BROOKE ON "THAT'S WHAT SHE SAID" ON THE THIEVES CAST CHANNEL.`,
     img: '/content/brookeab.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/brookeab' },
@@ -39,6 +47,8 @@ export const creators = [
   },
   {
     name: 'd4vd',
+    title: 'Creator',
+    bio: `A MEMBER OF 100 THIEVES CLASS OF 2024, D4VD IS A RENOWNED SINGER AND SONGWRITER BEST KNOWN FOR HIS SONGS "ROMANTIC HOMICIDE" AND "HERE WITH ME", BOTH OF WHICH EXPERIENCED INTERNATIONAL CHART SUCCESS.`,
     img: '/content/d4vd.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/d4vddd' },
@@ -47,6 +57,8 @@ export const creators = [
   },
   {
     name: 'Enable',
+    title: 'Creator',
+    bio: `A TIKTOK PHENOMENON WITH A PASSION FOR GAMING, VINNIE BRINGS A FUN AND FEARLESS SPIRIT THAT INSPIRES US ALL. HIS POSITIVE ENERGY FILLS EVERY ROOM & WE’RE THRILLED TO CREATE MORE CONTENT WITH VINNIE IN THE FUTURE IN GAMING AND BEYOND!`,
     img: '/content/enable.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/Enable' }
@@ -54,6 +66,8 @@ export const creators = [
   },
   {
     name: 'Fuslie',
+    title: 'Creator',
+    bio: `ALL OF US ARE LESLERS! VARIETY STREAMER WITH THE BRIGHTEST PERSONALITY, LESLIE HAS BEEN WITH 100THIEVES SINCE 2021!`,
     img: '/content/fuslie.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/fuslie' },
@@ -63,6 +77,8 @@ export const creators = [
   },
   {
     name: 'Hiko',
+    title: 'Creator',
+    bio: `A COUNTER-STRIKE ICON AND FORMER MEMBER OF 100 THIEVES VALORANT, HIKO IS BEST KNOWN FOR HIS INHUMAN REACTIONS AND INSANE CLUTCHES. HE NOW STREAMS VALORANT FOR 100 THIEVES.`,
     img: '/content/hiko.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/Hiko' },
@@ -72,6 +88,8 @@ export const creators = [
   },
   {
     name: 'iiTzTimmy',
+    title: 'Creator',
+    bio: `FPS GAMING LEGEND, MARATHON TWITCH STREAMER, HIGROUND AMBASSADOR. YOU CAN FIND FPS GAMING LEGEND TIMMY “IITZTIMMY” AN AT THE TOP OF THE LEADERBOARDS FOR ANY IN-SEASON FPS TITLE. TIMMY HAS AN UNCANNY ABILITY TO CLICK HEADS, AND HAS DEVELOPED A RABID FANBASE THAT FOLLOW HIM FROM FPS TITLE TO FPS TITLE, AS HE CONQUERS THEM ALL WITH EASE.`,
     img: '/content/iitztimmy.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/iiTzTimmy' },
@@ -82,6 +100,8 @@ export const creators = [
   },
   {
     name: 'JHB',
+    title: 'Creator',
+    bio: `STARTED OUT AS AN INTERN BUT WORKED HIS WAY UP TO A FULL TIME CREATOR AND EDITOR. JHB EMBODIES THE 100T SPIRIT, TAKING WHAT'S NOT GIVEN TO HIM AND WORKING ON A WIDE VARIETY OF PROJECTS ACROSS THE COMPANY. HE'S ALSO A PASSIONATE CREATIVE WHO ENJOYS CREATING HIS OWN LONG FORM YOUTUBE VIDEOS.`,
     img: '/content/jhb.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/JhbTeam' },
@@ -91,6 +111,8 @@ export const creators = [
   },
   {
     name: 'Jesser',
+    bio: `LOS ANGELES-BASED CONTENT CREATOR WITH OVER 15 MILLION+ FOLLOWERS AND COUNTING ACROSS HIS SOCIALS. HE IS BEST KNOWN FOR HIS BASKETBALL AND CHALLENGES CONTENT. JESSER IS THE BIGGEST BASKETBALL CREATOR IN THE SPACE AND WORKS WITH ALL LEVELS OF THE GAME. HIS MAGNETIC PERSONALITY AND ENERGY SHINE THROUGH IN ANYTHING HE DOES!`,
+    title: 'Creator',
     img: '/content/jesser.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/Jesser' },
@@ -100,6 +122,8 @@ export const creators = [
   },
   {
     name: 'Jiedel',
+    title: 'Creator',
+    bio: `HIS WILD SUCCESS ON YOUTUBE AND IN THE NBA 2K SPACE LED TO HIM BECOMING A MEMBER OF 2HYPE. HE LIKES TO DESCRIBE HIS VIDEOS AS “2 OUNCES OF COMEDIC VALUE, AND 4 TABLESPOONS OF STUPIDITY.”`,
     img: '/content/jiedel.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/jiedel' },
@@ -109,6 +133,8 @@ export const creators = [
   },
   {
     name: 'JoshChx',
+    bio: `A MEMBER OF 100 THIEVES CLASS OF 2024, JOSHCHX IS A PROMINENT FIGURE ON TIKTOK THROUGH BOTH HIS PERSONAL CHANNEL AND THE LA THIEVES ACCOUNT THAT HE RUNS ENTIRELY.`,
+    title: 'Creator',
     img: '/content/joshchx.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/joshchx' }
@@ -116,6 +142,8 @@ export const creators = [
   },
   {
     name: 'LEGIQN',
+    bio: `A MEMBER OF 100 THIEVES CLASS OF 2024, LEGIQN IS BEST KNOWN FOR HIS HUMOROUS GAMING CONTENT SURROUNDING FPS GAMES LIKE CALL OF DUTY AND FORTNITE.`,
+    title: 'Creator',
     img: '/content/legiqn.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/LEGIQN' },
@@ -126,6 +154,8 @@ export const creators = [
   },
   {
     name: 'Lidyuh',
+    title: 'Creator',
+    bio: `A MEMBER OF 100 THIEVES CLASS OF 2024, LIDYUH FOUND WILD SUCCESS IN HER FIRST FPS GAME, VALORANT. SHE COMPETED PROFESSIONALLY IN THE VALORANT GAME CHANGERS SCENE, THE CIRCUIT FOR WOMEN AND MARGINALIZED GENDERS.`,
     img: '/content/lidyuh.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/itslidyuh' },
@@ -134,6 +164,8 @@ export const creators = [
   },
   {
     name: 'Lough',
+    title: 'Creator',
+    bio: `A TIKTOK STAR KNOWN FOR HIS GAMING X LIFESTYLE CONTENT. LOUGH FOCUSES ON THE AESTHETICS AND QUALITY OF HIS GAMING SETUP AND PEELING BACK THE CURTAIN ON THE LIFESTYLE OF A GAMING INFLUENCER.`,
     img: '/content/lough.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/loughtv' },
@@ -144,6 +176,8 @@ export const creators = [
   },
   {
     name: 'NiceWigg',
+    title: 'Creator',
+    bio: `APEX STAR AND ENTHUSIST. NICEWIGG IS A KIND HEARTED SOUL WHO HAS BEEN A STAPLE ACROSS THE APEX SCENE FOR YEARS. HE CAN BE FOUND LEADING WATCH PARTIES OF ANY APEX TOURNAMENT AS WELL AS WORKING OUT IN THE GYM.`,
     img: '/content/nicewigg.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/NiceWigg' },
@@ -152,6 +186,8 @@ export const creators = [
   },
   {
     name: 'NoahJ',
+    title: 'Creator',
+    bio: `NOAH IS A LONGTIME YOUTUBE CREATOR WITH POPULAR PAYTHROUGH AND COD ZOMBIE STREAMS.NOAH ALSO HAS TAKEN TO A CAREER AS A DJ, JETSETTING ALL AROUND THE US TO SHOW OFF HIS SKILLS!`,
     img: '/content/noahj.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/noahj456' },
@@ -162,6 +198,8 @@ export const creators = [
   },
   {
     name: 'Octane',
+    title: 'Creator',
+    bio: `DIDN'T WE TELL YOU WE MAKE CHAMPIONS? A FORMER MEMBER OF LA THIEVES, OUR CALL OF DUTY DIVISION, OCTANE IS A WORLD CHAMPION WHO NOW STREAMS FOR 100 THIEVES.`,
     img: '/content/octane.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/OctaneSam' },
@@ -169,6 +207,8 @@ export const creators = [
   },
   {
     name: 'PeterPark',
+    title: 'Creator',
+    bio: `PETER'S JUST CHATTING STREAMS ON TWITCH NEVER CEASE TO MAKE HIS VIEWERS LAUGH. SINCE HIS JOINING WITH 100 THIEVES IN AUGUST OF 2022, HE'S HOSTED HIS OWN PODCAST TITLED BOOMER VS ZOOMER, FEATURING GENUINE, UNFILTERED CONVERSATIONS BETWEEN CREATORS OF ALL AGES.`,
     img: '/content/peterpark.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/peterparkTV' },
@@ -178,6 +218,8 @@ export const creators = [
   },
   {
     name: 'Sparkles',
+    title: 'Creator',
+    bio: `A MEMBER OF 100 THIEVES CLASS OF 2024, SPARKLES, ALSO KNOWN AS SPARKLES_QT, IS BEST KNOWN FOR HER FORTNITE CONTENT, WHICH IS FILLED TO THE BRIM WITH EQUAL PARTS ENTERTAINMENT AND EQUAL PARTS COMPETITION.`,
     img: '/content/sparkles.jpeg',
     socials: [
       { platform: 'twitch', link: 'https://www.twitch.tv/sparkles_qt' },
@@ -187,6 +229,8 @@ export const creators = [
   },
   {
     name: 'Vinnie',
+    title: 'Creator',
+    bio: `A TIKTOK PHENOMENON WITH A PASSION FOR GAMING, VINNIE BRINGS A FUN AND FEARLESS SPIRIT THAT INSPIRES US ALL. HIS POSITIVE ENERGY FILLS EVERY ROOM & WE’RE THRILLED TO CREATE MORE CONTENT WITH VINNIE IN THE FUTURE IN GAMING AND BEYOND!`,
     img: '/content/vinnie.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/vinniehacker' },
@@ -197,6 +241,8 @@ export const creators = [
   },
   {
     name: 'Woolz',
+    title: 'Creator',
+    bio: `A MEMBER OF 100 THIEVES CLASS OF 2024, KINGWOOLZ IS A SELF-PROCLAIMED FORMER HOCKEY PLAYER WITH A PASSION FOR ENTERTAINMENT.`,
     img: '/content/woolz.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/kingwoolz' },
@@ -206,6 +252,8 @@ export const creators = [
   },
   {
     name: 'Yassuo',
+    title: 'Creator',
+    bio: `ORIGINALLY MAKING HIS TWITCH CAREER AS A TOP-TIER YASUO PLAYER IN LEAGUE OF LEGENDS IN 2015, MOE'S INTERNET PERSONALITY HAS ALLOWED HIM TO TRAVERSE A PLETHORA OF GAMING GENRES, BECOMING A POPULAR VARIETY STREAMER IN THE PRESENT DAY.`,
     img: '/content/yassuo.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/Yassuo' },
@@ -215,6 +263,8 @@ export const creators = [
   },
   {
     name: 'ZackTTG',
+    title: 'Creator',
+    bio: `PREVIOUS ENGINEER TURNED YOUTUBE VLOGGER, ZACK EMBODIES ATHLETICISM, SMARTS, AND "ALWAYS BE READY ATTITUDE".`,
     img: '/content/zackttg.jpeg',
     socials: [
       { platform: 'twitter', link: 'https://twitter.com/ZackTTG' },
