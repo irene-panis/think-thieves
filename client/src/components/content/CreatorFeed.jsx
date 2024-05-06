8d2b79add8f5ca6c7f70600f86c9ec5664b5f2cb
irene <panisirene@gmail.com>
2024-05-06 14:07:32 -0700

    fixing spacing

--- client/src/components/content/CreatorFeed.jsx
+++ client/src/components/content/CreatorFeed.jsx
@@ -2,8 +2,8 @@ import { WhosLive } from "../esports/WhosLive"
 
 export const CreatorFeed = () => {
   return (
-    <>
+    <div className="creator-feed m-2">
       <WhosLive team="CONTENT"/>
-    </>
+    </div>
   )
 }
