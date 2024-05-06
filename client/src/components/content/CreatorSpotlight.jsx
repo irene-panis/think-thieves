7bf8d8f4f4f7ff9b092f6031ad08e7c8abdb9167
irene <panisirene@gmail.com>
2024-05-05 19:26:46 -0700

    add space between icons

--- client/src/components/content/CreatorSpotlight.jsx
+++ client/src/components/content/CreatorSpotlight.jsx
@@ -3,11 +3,11 @@ import { SocialIcon } from "../SocialIcon";
 
 export const CreatorSpotlight = ( { creator } ) => {
   return (
-    <div className="creator-spotlight mx-4 flex-grow">
+    <div className="creator-spotlight mx-4 flex-grow flex flex-col gap-2">
       <Image src={creator.img}/>
       <div className="creator-info flex justify-between">
         <span className="uppercase">{creator.name}</span>
-        <div className="creator-socials">
+        <div className="creator-socials flex gap-2">
           {creator.socials.map((social, index) => {
             return (
               <span key={index}>
