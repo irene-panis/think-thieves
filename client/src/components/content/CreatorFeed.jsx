da36c9ce3f031a5d9481352e75d41ccc0f37d199
irene <panisirene@gmail.com>
2024-05-06 14:09:59 -0700

    make creator feed flex wrap

--- client/src/components/content/CreatorFeed.jsx
+++ client/src/components/content/CreatorFeed.jsx
@@ -2,7 +2,7 @@ import { WhosLive } from "../esports/WhosLive"
 
 export const CreatorFeed = () => {
   return (
-    <div className="creator-feed m-2">
+    <div className="creator-feed flex flex-wrap m-2">
       <WhosLive team="CONTENT"/>
     </div>
   )
