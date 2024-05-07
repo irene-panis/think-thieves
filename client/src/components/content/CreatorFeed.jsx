bd2bbb880877e7adcc07f100fc7645877e41be5c
irene <panisirene@gmail.com>
2024-05-06 17:11:35 -0700

    create creatorfeed and add WhosLive component

--- client/src/components/content/CreatorFeed.jsx
+++ client/src/components/content/CreatorFeed.jsx
@@ -2,8 +2,8 @@ import { WhosLive } from "../esports/WhosLive"
 
 export const CreatorFeed = () => {
   return (
-    <div className="creator-feed flex flex-wrap m-4">
+    <>
       <WhosLive team="CONTENT"/>
-    </div>
+    </>
   )
 }
