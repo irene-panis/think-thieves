002cb219c7ffffb40e1827ba06c1d57812da6a31
irene <panisirene@gmail.com>
2024-05-07 12:42:56 -0700

    create merch carousel with swiper, no text yet

--- client/src/components/merch/MerchCarousel.jsx
+++ client/src/components/merch/MerchCarousel.jsx
@@ -1,5 +1,30 @@
+import { useRef, useEffect } from "react";
+import { register } from 'swiper/element/bundle';
+
+register();
+
 export const MerchCarousel = ({ images }) => {
+  const swiperElRef = useRef(null);
+
+  const containerHeight = `calc(100vh - 4rem)`;
+
   return (
-    <></>
-  )
+    <swiper-container
+      ref={swiperElRef}
+      slides-per-view="1"
+      navigation="true"
+      pagination="true"
+      loop="true"
+      autoplay-delay="5000"
+      speed="500"
+    >
+      {
+        images.map((img, index) => (
+          <swiper-slide key={index}>
+            <img className="w-full h-full object-cover" src={img}/>
+          </swiper-slide>
+        ))
+      }
+    </swiper-container>
+  );
 };
