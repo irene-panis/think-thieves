a9e42b50e41f15534a7a8a181e6439abc656edb4
irene <panisirene@gmail.com>
2024-05-06 20:43:29 -0700

    remove btns from carousel, add interval

--- client/src/components/merch/MerchCarousel.jsx
+++ client/src/components/merch/MerchCarousel.jsx
@@ -1,52 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 export const MerchCarousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex + 1 === images.length ? 0 : prevIndex + 1
-    );
-  };
-  const handlePrevious = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
   const handleDotClick = (index) => {
     setCurrentIndex(index);
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Increment currentIndex and reset to 0 if it exceeds the maximum index
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % 4);
+    }, 5000);
+  
+    return () => clearInterval(interval);
+  }, [])
+
   return (
-    <div className="carousel">
-      <img key={currentIndex} src={images[currentIndex]} />
-      <div className="slide_direction">
-        <div className="left" onClick={handlePrevious}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="20"
-            viewBox="0 96 960 960"
-            width="20"
-          >
-            <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
-          </svg>
-        </div>
-        <div className="right" onClick={handleNext}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="20"
-            viewBox="0 96 960 960"
-            width="20"
-          >
-            <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
-          </svg>
-        </div>
-      </div>
-      <div className="indicator">
+    <div className="carousel flex flex-col items-center h-full relative bg-zinc-900/50">
+      <img className="w-full h-full object-cover" key={currentIndex} src={images[currentIndex]} />
+      <div className="carousel-indicator">
         {images.map((_, index) => (
           <div
             key={index}
-            className={`dot ${currentIndex === index ? "active" : ""}`}
+            className={`dot ${currentIndex === index ? "active-img" : ""}`}
             onClick={() => handleDotClick(index)}
           ></div>
         ))}
--- client/src/pages/Apparel.jsx
+++ client/src/pages/Apparel.jsx
@@ -1,7 +1,16 @@
+import { MerchCarousel } from "../components/merch/MerchCarousel"
+
 export const Apparel = () => {
+  const carouselImgs = [
+    '/apparel/foundations-merch.jpeg',
+    '/apparel/vct-merch.jpeg',
+    '/apparel/pokemon-merch.jpeg',
+    '/apparel/lat-merch.jpg'
+  ];
+  const containerHeight = `calc(100vh - 4rem)`;
   return (
-    <div className="apparel-wrapper w-full h-full">
-      <span>apparel tab</span>
+    <div className="apparel-wrapper w-full" style={{ height: containerHeight }}>
+      <MerchCarousel images={carouselImgs}/>
     </div>
   )
 }
