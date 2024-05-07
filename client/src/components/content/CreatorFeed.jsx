d2003ae76045a416c850af4eedb38a320e46aec7
irene <panisirene@gmail.com>
2024-05-06 17:18:18 -0700

    add back the margins i removed oops

--- client/src/components/content/CreatorFeed.jsx
+++ client/src/components/content/CreatorFeed.jsx
@@ -2,8 +2,8 @@ import { WhosLive } from "../esports/WhosLive"
 
 export const CreatorFeed = () => {
   return (
-    <>
+    <div className="m-4">
       <WhosLive team="CONTENT"/>
-    </>
+    </div>
   )
 }
