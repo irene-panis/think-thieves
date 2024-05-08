6cdaacea27b266aaf284fb89247755ab82068c4f
irene <panisirene@gmail.com>
2024-05-08 15:15:09 -0700

    wrap merch carousel inside div to give it a class name

--- client/src/components/merch/MerchCarousel.jsx
+++ client/src/components/merch/MerchCarousel.jsx
@@ -7,26 +7,31 @@ export const MerchCarousel = ({ images, onApparelChange }) => {
   const swiperElRef = useRef(null);
 
   return (
-    <swiper-container
-      ref={swiperElRef}
-      slides-per-view="1"
-      navigation="true"
-      pagination="true"
-      loop="true"
-      autoplay-delay="5000"
-      speed="500"
-    >
-      {
-        images.map((img, index) => (
+    <div className="merch-carousel">
+      <swiper-container
+        ref={swiperElRef}
+        slides-per-view="1"
+        navigation="true"
+        pagination="true"
+        loop="true"
+        autoplay-delay="5000"
+        speed="500"
+      >
+        {images.map((img, index) => (
           <swiper-slide key={index}>
             <div className="slide-text absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full text-center drop-shadow-md text-white">
               <h2 className="font-bold font-dmmono text-3xl">{img.text}</h2>
-              <span className="underline hover:bg-black hover:text-white duration-200 ease-in-out" onClick={() => onApparelChange(img.text)}>LEARN MORE</span>
+              <span
+                className="underline hover:bg-black hover:text-white duration-200 ease-in-out"
+                onClick={() => onApparelChange(img.text)}
+              >
+                LEARN MORE
+              </span>
             </div>
-            <img className="w-full h-full object-cover" src={img.img}/>
+            <img className="w-full h-full object-cover" src={img.img} />
           </swiper-slide>
-        ))
-      }
-    </swiper-container>
+        ))}
+      </swiper-container>
+    </div>
   );
 };
