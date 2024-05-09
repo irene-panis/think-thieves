beecc98f54c5b197a7269ae606ffc61453118cf1
irene <panisirene@gmail.com>
2024-05-09 15:04:49 -0700

    add featured products for jersey

--- client/src/components/merch/Jersey.jsx
+++ client/src/components/merch/Jersey.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import { useRef } from "react";
 import { register } from 'swiper/element/bundle';
+import { FeaturedProducts } from "./FeaturedProducts";
 
 register();
 
@@ -11,6 +12,14 @@ export const Jersey = () => {
     '/apparel/jersey/two.jpeg',
     '/apparel/jersey/three.jpeg',
   ];
+
+  const featured = [
+    {
+      path: '/apparel/jersey/featured1.jpeg',
+      hover: `/apparel/jersey/featured1b.jpeg`,
+      name: `ADIDAS ORIGINALS X 100 THIEVES 2024 PRIMARY JERSEY`
+    }
+  ]
 
   return (
     <>
@@ -23,21 +32,23 @@ export const Jersey = () => {
           effect="slide"
         >
           <swiper-slide>
-            <div className="font-oswald font-bold text-[#131200] uppercase text-4xl tracking-[0.5em] mr-[-0.5em] translate-y-3">Everyday</div>
+            <div className="font-oswald font-bold text-[#131200] uppercase text-4xl tracking-[0.5em] mr-[-0.5em]">Game Day</div>
             <img className="object-cover h-1/2 w-2/3" src={images[0]} />
-            <div className="font-oswald text-[#131200] uppercase text-4xl tracking-[0.5em] mr-[-0.5em] font-bold">Styles</div>
+            <div className="font-oswald text-[#131200] uppercase text-4xl tracking-[0.5em] mr-[-0.5em] font-bold">Ready</div>
           </swiper-slide>
           <swiper-slide>
-            <div className="text-center w-[80%] -translate-y-3">Drawing inspiration from classic athleticwear and utilitarian tech wear.</div>
+            <div className="text-center w-[80%] -translate-y-3">Official team issued jerseys, from 100T and adidas.</div>
             <img className="object-cover h-1/2 w-2/3" src={images[1]} />
+            <div className="text-center w-[80%] translate-y-3">Boxy fit, ribbed collars, embroidered logos, iconic adidas stripes.</div>
           </swiper-slide>
           <swiper-slide>
-            <div className="lowercase">Accessible products //</div>
+            <div className="lowercase">Maximum comfort //</div>
             <img className="object-cover h-1/2 w-2/3" src={images[2]} />
-            <div className="lowercase">// True to heritage</div>
+            <div className="lowercase">// Meets aesthetic</div>
           </swiper-slide>
         </swiper-container>
       </div>
+      <FeaturedProducts productsArray={featured}/>
     </>
   );
 }
