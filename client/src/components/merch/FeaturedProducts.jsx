08f61731498a2328abf4d48a326ebbd0f99c989a
irene <panisirene@gmail.com>
2024-05-09 14:42:37 -0700

    add media queries / tweak spacing

--- client/src/components/merch/FeaturedProducts.jsx
+++ client/src/components/merch/FeaturedProducts.jsx
@@ -3,7 +3,7 @@ import { Header } from "../Header"
 
 export const FeaturedProducts = ({ productsArray }) => {
   return (
-    <div className="featured-container flex flex-col gap-2 p-4 items-center">
+    <div className="featured-container flex flex-col gap-2 m-4 items-center">
       <Header header="Featured Products"/>
       <div className="products-container flex flex-wrap justify-center gap-4">
         {
