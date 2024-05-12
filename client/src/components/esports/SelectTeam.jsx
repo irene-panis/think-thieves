c927a21e271e9a50bb27df2894de3aab385da8bf
irene <panisirene@gmail.com>
2024-05-11 20:28:41 -0700

    make team images slant/overlap only on large screen sizes, column by default

--- client/src/components/esports/SelectTeam.jsx
+++ client/src/components/esports/SelectTeam.jsx
@@ -5,13 +5,13 @@ export const SelectTeam = ( props ) => {
   
   return (
     <div className='select-team flex flex-col lg:flex-row w-full overflow-hidden text-white relative' style={{ height: containerHeight }}>
-      <div className="h-full w-full flex flex-col justify-center items-center bg-val bg-cover bg-center">
+      <div className="h-full w-full flex flex-col justify-center items-center bg-val bg-cover bg-center slanted-item">
         <TeamButton onTeamChange={props.onTeamChange}>VALORANT</TeamButton>
       </div>
-      <div className="h-full w-full flex flex-col justify-center items-center bg-lcs bg-cover bg-center">
+      <div className="h-full w-full flex flex-col justify-center items-center bg-lcs bg-cover bg-center slanted-item">
         <TeamButton onTeamChange={props.onTeamChange}>LEAGUE OF LEGENDS</TeamButton>
       </div>
-      <div className="h-full w-full flex flex-col justify-center items-center bg-cdl bg-cover bg-center">
+      <div className="h-full w-full flex flex-col justify-center items-center bg-cdl bg-cover bg-center slanted-item">
         <TeamButton onTeamChange={props.onTeamChange}>CALL OF DUTY</TeamButton>
       </div>
   </div>
