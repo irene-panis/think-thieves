52a2da88b99ab3ecf163ee7dc919a04f1bc0318a
irene <panisirene@gmail.com>
2024-05-13 17:51:00 -0700

    add carousel images for wider screen sizes

--- client/src/pages/Apparel.jsx
+++ client/src/pages/Apparel.jsx
@@ -6,18 +6,22 @@ export const Apparel = () => {
   const carouselImgs = [
     {
       img: '/apparel/foundations-merch.jpeg',
+      img_lg: '/apparel/foundations2.jpeg',
       text: `FOUNDATIONS SS'24`
     },
     {
       img: '/apparel/vct-merch.jpeg',
+      img_lg: '/apparel/jersey/one.jpeg',
       text: `2024 PRIMARY JERSEY`
     },
     {
       img: '/apparel/pokemon-merch.jpeg',
+      img_lg: '/apparel/pokemon2.jpeg',
       text: `100T x POKEMON`
     },
     {
       img: '/apparel/lat-merch.jpg',
+      img_lg: '/apparel/lat2.jpeg',
       text: `LA THIEVES`
     },
   ];
