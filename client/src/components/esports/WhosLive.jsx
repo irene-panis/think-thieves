a62f48a828217c2ca5b090d63f77687b1439ff2e
irene <panisirene@gmail.com>
2024-05-14 19:05:36 -0700

    change fetch urls to match heroku backend deployment

--- client/src/components/esports/WhosLive.jsx
+++ client/src/components/esports/WhosLive.jsx
@@ -8,7 +8,7 @@ export const WhosLive = ( props ) => {
 
   useEffect(() => {
     const grabData = async () => {
-      const getURL = `/api/get-streams/${props.team}`;
+      const getURL = `https://thinkthieves-c78e2959a576.herokuapp.com/api/get-streams/${props.team}`;
       const response = await fetch(getURL, {
         method: "GET",
         headers: {
