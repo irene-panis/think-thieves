b532f40b4a392a4b8d55520dffa81ef181827fcd
irene <panisirene@gmail.com>
2024-05-15 16:08:19 -0700

    remove horizontal margin

--- client/src/components/content/CreatorFeed.jsx
+++ client/src/components/content/CreatorFeed.jsx
@@ -2,7 +2,7 @@ import { WhosLive } from "../esports/WhosLive"
 
 export const CreatorFeed = () => {
   return (
-    <div className="m-4">
+    <div className="my-4">
       <WhosLive team="CONTENT"/>
     </div>
   )
