fc4729c49be9f51d2b151c6673e306cc735fea23
irene <panisirene@gmail.com>
2024-05-14 19:38:53 -0700

    add cors origin

--- server/server.js
+++ server/server.js
@@ -19,7 +19,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://think-thieves.vercel.app'
+}));
 
 const client = createClient({
   url: process.env.REDIS_TLS_URL,
